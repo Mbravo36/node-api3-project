0f796f0c9a02a01a200a9f229f8856a395aff082
Your Name <mari@Maribels-MacBook-Pro.local>
2022-03-09 23:15:16 -0600

    have been commiting to the wrong project this whole time, but I finished and completed mvp, all test passing

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,5 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
   const timestamp = new Date().toLocaleString()
@@ -7,22 +9,45 @@ function logger(req, res, next) {
   next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
-  console.log("validateUserId middleware")
-  next()
+async function validateUserId(req, res, next) {
+  try{
+    const user = await User.getById(req.params.id)
+    if (!user) {
+      next({ status: 404, message: 'user not found'})
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'problem finding user',
+    })
+  }
+  
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
-  console.log("validateUser middleware")
-  next()
+  const { name } = req.body
+  if (!name || !name.trim()) {
+    res.status(400).json({
+      message: 'missing required name'
+    })
+  } else {
+    req.name = name.trim()
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
-  console.log("validatePost middleware")
-  next()
+  const { text } = req.body
+  if (!text || !text.trim()) {
+    res.status(400).json({
+      message: 'missing required text'
+    })
+  } else {
+    req.text = text.trim()
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
